ec2677f636267e0736f21476dcee8dadff87adae
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 14:14:37 -0300

    Add files via upload

---
+++ servidor/run.sh
@@ -0,0 +1,15 @@
+if [ "$USER" = "root" ]; then
+	if [ "$1" = "apt" ]; then
+		echo "[$apt]: $up1"
+		$apt $up1
+		echo "[$apt]: $up2"
+		$apt $up2
+	fi
+	if [ "$servidoronfilebrowser" = "on" ]; then
+		./run/filebrowser.sh "$servidordatafilebrowser" "$servidoripfilebrowser" "$servidordoorfilebrowser"
+		#echo "1: $servidordatafilebrowser 2: $servidoripfilebrowser 3: $servidordoorfilebrowser"
+	fi
+else
+	echo "[error]: sudo"
+	echo "[error]: root"
+fi
