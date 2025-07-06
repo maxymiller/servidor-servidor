486fd0c79bbfef2b21c076c3ec67813cb8e9c537
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 17:56:35 -0300

    Update downloadlist.sh

--- download/downloadlist.sh
+++ download/downloadlist.sh
@@ -5,17 +5,33 @@ run="$home/run"
 criar="$home/criar"
 valor="maxy/valor"
 
+#home
+echo "[*]: upgrade-run.sh"
+curl -fsSL $home/upgrade-run.sh > upgrade-run.sh
+
 #config
+echo "[*]: dir config"
 curl -fsSL $config/run.sh > config/run.sh
 
 #criar
+echo "[*]: dir criar"
 curl -fsSL $criar/filebrowser.sh > criar/filebrowser.sh
 
 #run
+echo "[*]: dir run"
 curl -fsSL $run/filebrowser.sh > run/filebrowser.sh
 
+#maxy
+echo "[*] dir maxy"
+curl -fsSL $home/maxy/t.txt > maxy/t.txt
+
 #valor
+echo "[*] dir maxy/valor(1/4)"
 curl -fsSL $home/$valor/servidordata.txt > $valor/servidordata.txt
+echo "[*] dir maxy/valor(2/4)"
 curl -fsSL $home/$valor/servidordatafilebrowser.txt > $valor/servidordatafilebrowser.txt
+echo "[*] dir maxy/valor(3/4)"
 curl -fsSL $home/$valor/servidorhome.txt > $valor/servidorhome.txt
+echo "[*] dir maxy/valor(4/4)"
 curl -fsSL $home/$valor/servidorhomefilebrowser.txt > $valor/servidorhomefilebrowser.txt
+echo "[download]: done"
