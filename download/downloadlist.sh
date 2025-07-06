8faf8281101c31373bf9797279c91a69bec1a847
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 17:37:29 -0300

    Update downloadlist.sh

--- download/downloadlist.sh
+++ download/downloadlist.sh
@@ -1,1 +1,21 @@
-curl https://maxymiller-servidor.netlify.app/servidor/config/run.sh -fsSL > config/run.sh
+url="https://maxymiller-servidor.netlify.app"
+home="$url/servidor"
+config="$home/config"
+run="$home/run"
+criar="$home/criar"
+valor="maxy/valor"
+
+#config
+curl -fsSL $config/run.sh > config/run.sh
+
+#criar
+curl -fsSL $criar/filebrowser.sh > criar/filebrowser.sh
+
+#run
+curl -fsSL $run/filebrowser.sh > run/filebrowser.sh
+
+#valor
+curl -fsSL $home/$valor/servidordata.txt > $valor/servidordata.txt
+curl -fsSL $home/$valor/servidordatafilebrowser.txt > $valor/servidordatafilebrowser.txt
+curl -fsSL $home/$valor/servidorhome.txt > $valor/servidorhome.txt
+curl -fsSL $home/$valor/servidorhomefilebrowser.txt > $valor/servidorhomefilebrowser.txt
