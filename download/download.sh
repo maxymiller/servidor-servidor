9b5ed7868ecb8574bc7e51ae5e96705ab39004f2
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 16:29:30 -0300

    Update download.sh

--- download/download.sh
+++ download/download.sh
@@ -1,14 +1,14 @@
 cmdsh1="mkdir servidor"
 cmdsh2="cd servidor"
-cmdsh3="curl -fsSL https://maxymiller-servidor.netlify.app/servidor/download.sh > download.sh"
+cmdsh3="curl -fsSL https://maxymiller-servidor.netlify.app/servidor/download.sh"
 cmdsh4="chmod 774 download.sh"
 
 echo "[*]: $cmdsh1"
 $cmdsh1
 echo "[*]: $cmdsh2"
 $cmdsh2
-echo "[*]: $cmdsh3"
-$cmdsh3
+#echo "[*]: $cmdsh3"
+$cmdsh3 > download.sh
 echo "[*]: $cmdsh4"
 $cmdsh4
 echo
