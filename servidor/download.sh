d28ce148365c36a3a8443e2a8513ed64e2ad736a
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 18:44:10 -0300

    Update download.sh

--- servidor/download.sh
+++ servidor/download.sh
@@ -78,13 +78,7 @@ if [ "$USER" = "root" ]; then
 	# fim
   	echo "[done]: ok"
    	echo
-	echo "[info]: config: [$PWD/$config]"
-	echo "[info]: servidor home: [$PWD/servidor]"
-	echo "[info]: servidor data: [$PWD/data-servidor-config]"
-	echo ""
-	echo "[info]: servidor filebrowser home: [$PWD/servidor/filebrowser]"
-	echo "[info]: servidor data filebrowser: [$PWD/data-servidor-config/filebrowser]"
-	echo "[info]: download filebrowser"
+	curl -fsSL https://maxymiller-servidor.netlify.app/download/downloaddone.sh
 else
 	echo "[error]: sudo download.sh"
 	echo "[user]: você não e root"
