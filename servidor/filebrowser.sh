fabf95170bea48a3f5d88d6990f3d0413e372e46
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 14:17:57 -0300

    Add files via upload

---
+++ servidor/filebrowser.sh
@@ -0,0 +1,18 @@
+config="config/config.txt"
+configdata="criar/data/filebrowser.txt"
+maxyt="$(cat maxy/t.txt)"
+servidorhomefilebrowser="$(cat maxy/valor/servidorhomefilebrowser.txt)"
+servidordatafilebrowser="$(cat maxy/valor/servidordatafilebrowser.txt)"
+if [ "$USER" = "root" ]; then
+	if [ -e "$config" ]; then
+		cp "$config" "$configdata"
+		echo >> "$configdata"
+		echo "cd $maxyt""$servidordatafilebrowser""$maxyt" >> "$configdata"
+		echo "filebrowser -r ""$maxyt""$servidorhomefilebrowser""$maxyt" >> "$configdata"
+		chmod 774 "$configdata"
+		./$configdata
+	fi
+else
+	echo "[error]: sudo"
+	echo "[error]: root"
+fi
