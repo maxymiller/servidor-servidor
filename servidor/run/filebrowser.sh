6cb03810c8ae0596f354b18721be247a4fc08390
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-06 23:00:57 -0300

    Update filebrowser.sh

--- servidor/run/filebrowser.sh
+++ servidor/run/filebrowser.sh
@@ -1,17 +1,22 @@
 # filebrowser.sh <file> <ip> <door>
-help="echo filebrowser.sh <dir data> <ip> <door>"
+help="echo filebrowser.sh <dir data> <ip> <door> <log>"
 filebrowserdata="$1"
 filebrowserip="$2"
 filebrowserdoor="$3"
+filebrowserlog="$4"
 if [ "$1" != "" ]; then
 	if [ "$2" != "" ]; then
 		if [ "$3" != "" ]; then
-			while [ "" = "" ]; do
-			cd "$filebrowserdata"
-				filebrowser -a $filebrowserip -p $filebrowserdoor
-				#echo "1: $filebrowserdata 2: $filebrowserip 3: $filebrowserdoor"
-				sleep 3
-			done
+  			if [ "$4" != "" ]; then
+				while [ "" = "" ]; do
+				cd "$filebrowserdata"
+					filebrowser -a $filebrowserip -p $filebrowserdoor > "$filebrowserlog/filebrowser.log"
+					#echo "1: $filebrowserdata 2: $filebrowserip 3: $filebrowserdoor"
+					sleep 3
+				done
+			else
+				$help
+   			fi
 		else
 			$help
 		fi
