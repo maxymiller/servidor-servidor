f1e241def2cc9561d8b40745284d5f0315d41c52
maxymiller <152033482+maxymiller@users.noreply.github.com>
2025-07-17 22:40:24 -0300

    Update upgrade-run.sh

--- servidor/upgrade-run.sh
+++ servidor/upgrade-run.sh
@@ -4,8 +4,9 @@ maxyt="$(cat maxy/t.txt)"
 if [ "$USER" = "root" ]; then
 	if [ -e "$filerun" ]; then
 		if [ -e "$filerun2" ]; then
-			echo "[cp]: $filerun run.sh"
-			cp "$filerun" "run.sh"
+			#echo "[cp]: $filerun run.sh"
+			#cp "$filerun" "run.sh"
+   			echo "source $filerun" > run.sh
 			echo >> run.sh
 			echo "[cat]: $filerun2"
 			cat "$filerun2" >> run.sh
